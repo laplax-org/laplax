6121493598b29c5a0a9c7b8ccffc234a6669bd83
Luis Gindorf <luis.gindorf@github.com>
2025-10-31 12:06:54 +0100

    First idea of empirical fisher calculation

--- laplax/curv/fisher.py
+++ laplax/curv/fisher.py
@@ -6,7 +6,7 @@ import jax
 
 from loguru import logger
 
-from laplax.curv.loss import fetch_loss_hessian_mv
+from laplax.curv.loss import fetch_loss_gradient_fn
 
 
 from laplax.enums import LossFn
@@ -27,13 +27,11 @@ def create_fisher_mv_without_data(
 	factor: Float,
 	*,
 	vmap_over_data: bool = True,
-	loss_hessian_mv: Callable | None = None,
+	loss_gradient_fn: Callable | None = None,
 	mc_samples: Int | None = None,
 ) -> Callable[[Params, Data], Params]:
 	r"""Create Fisher matrix-vector product without fixed data.
-
-    ## TODO: Add mathy description
-
+	Uses either Empirical or Monte Carlo approximation.
     #$$
     #\text{factor} \cdot \sum_i J_i^\top \nabla^2_{f(x_i, \theta), f(x_i, \theta)}
     #\mathcal{L}(f(x_i, \theta), y_i) J_i \cdot v
@@ -47,14 +45,14 @@ def create_fisher_mv_without_data(
     dataset, making it suitable for distributed or batched computations.
 
     Args:
-    	type: The type of Fisher approximation.
+    	type: The type of Fisher approximation. Either 'EMPIRICAL' or 'MC'.
         model_fn: The model's forward pass function.
         params: Model parameters.
         loss_fn: Loss function to use for the Fisher computation.
         factor: Scaling factor for the Fisher computation.
         vmap_over_data: Whether to vmap over the data. Defaults to True.
-        loss_hessian_mv: The loss Hessian matrix-vector product.
-		mc_samples: Number of MC samples to use for type "MC". Defaults to 1.
+        loss_grad_fn: The loss gradient function.
+		mc_samples: Number of MC samples to use for type 'MC'. Defaults to 1.
 
     Returns:
         A function that takes a vector and a batch of data, and computes the Fisher
@@ -65,20 +63,34 @@ def create_fisher_mv_without_data(
 
     """
 
-    # Create loss Hessian-vector product
-	loss_hessian_mv = fetch_loss_hessian_mv(loss_fn, loss_hessian_mv, vmap_over_data)
-
-	if vmap_over_data:
-		loss_hessian_mv = jax.vmap(loss_hessian_mv)
+	# Create loss gradient product
+	loss_grad_fn = fetch_loss_gradient_fn(loss_fn, loss_gradient_fn, vmap_over_data)
 
 	def empirical_fisher_mv(vec, data):
-		raise NotImplementedError
+		def fwd(p):
+			# Step 1: Single jvp for entire batch, if vmap_over_data is True
+			if vmap_over_data:
+				return jax.vmap(lambda x: model_fn(input=x, params=p))(data["input"])
+			return model_fn(input=data["input"], params=p)
+
+		# Step 2: Linearize the forward pass
+		z, jvp = jax.linearize(fwd, params)
+
+		grad = loss_grad_fn(fwd(params), data["target"])
+		vjp = jax.linear_transpose(jvp, vec)
+
+		Jv = jvp(vec)
+		GtJv = grad.T @ Jv
+		GGtJv = grad @ GtJv
+		JtGGtJv = vjp(GGtJv)
+		return JtGGtJv
 
 	def mc_fisher_mv(vec, data):
 		raise NotImplementedError
 
 	if type == FisherType.EMPIRICAL:
 		if mc_samples is not None:
+			# This is not an error (does not prevent computation), but is likely unintended -> Warning
 			logger.warning("Parameter 'mc_samples' does not affect FisherType 'EMPIRICAL'. Did you mean to use FisherType 'MC'?")
 		return empirical_fisher_mv
 
