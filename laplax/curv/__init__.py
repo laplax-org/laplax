c657a8b7de6f6c2ca4ff5819b2ed34d9585f91f7
Luis Gindorf <luis.gindorf@gmail.com>
2026-01-16 14:21:19 +0100

    Add fisher functions to submodule __init__

--- laplax/curv/__init__.py
+++ laplax/curv/__init__.py
@@ -7,9 +7,16 @@ from .cov import (
 )
 from .ggn import create_ggn_mv
 
+from .fisher import (
+    create_empirical_fisher_mv,
+    create_MC_fisher_mv
+    )
+
 __all__ = [
     "create_ggn_mv",
     "create_posterior_fn",
     "estimate_curvature",
     "set_posterior_fn",
+    "create_empirical_fisher_mv",
+    "create_MC_fisher_mv"
 ]
