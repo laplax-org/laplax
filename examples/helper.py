6c4b96b2e080733a75f6a576cd0f793121ecc58c
Luis Gindorf <luis.gindorf@student.uni-tuebingen.de>
2026-01-27 16:33:48 +0100

    Implement active learning loop

--- examples/helper.py
+++ examples/helper.py
@@ -33,6 +33,11 @@ class DataLoader:
 
         return self.X[batch_indices], self.y[batch_indices]
 
+    def add(self, x, y):
+        new_X = jnp.concatenate((self.X, jnp.atleast_2d(x)))
+        new_Y = jnp.concatenate((self.y, jnp.atleast_2d(y)))
+        return DataLoader(new_X, new_Y, batch_size = self.batch_size, shuffle=self.shuffle)
+
 
 DEFAULT_INTERVALS = [
     (0, 2),
