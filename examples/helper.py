6f2bfecc12aedafec50ac4fb057e7df8cab4a3a3
Luis Gindorf <luis.gindorf@gmail.com>
2026-01-29 10:34:42 +0100

    Some small fixes

--- examples/helper.py
+++ examples/helper.py
@@ -40,6 +40,9 @@ class DataLoader:
             new_X, new_Y, batch_size=self.batch_size, shuffle=self.shuffle
         )
 
+    def __len__(self):
+        return self.dataset_size
+
 
 DEFAULT_INTERVALS = [
     (0, 2),
